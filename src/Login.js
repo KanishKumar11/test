e98ed64a70d47c90d0c6b67862095de659c8f846
Kanish Kumar <kanishkumar11@outlook.in>
2023-05-27 22:43:30 +0530

    testt

--- src/Login.js
+++ src/Login.js
@@ -43,13 +43,26 @@ function Login() {
 
 
     return (
-        <div className = "login">
+        <div className = "login relative overflow-hidden">
             <div className = "login__logo">
-                <h2 className='text-5xl'>Classify</h2>
+
+                <h2 className='text-5xl text-white'>Classify</h2>
+            </div>
+            <div className='flex text-white text-[30px] font-[800] self-start items-start w-full ml-20 lg:ml-[12rem] lg:text-[40px] '>
+            <div className='text-gray-300'>
+              Explore
+            </div>
+            <div>
+              Education
+            </div>
             </div>
             <Button 
                 onClick = {signIn}
-                className ="login__button">Sign In</Button>
+                className ="login__button bg-blue-600 rounded-lg py-10">Sign In With Google</Button>
+            <video autoplay loop muted className='absolute inset-0 w-full h-full object-cover z-10' >
+              <source src="../public/back.mp4" type='video/mp4'/>
+              Your browser not supports video
+              </video>
         </div>
     )
 }
